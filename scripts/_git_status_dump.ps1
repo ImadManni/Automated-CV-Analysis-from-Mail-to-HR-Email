2c8fc85f1ae8cbca88b9a65273ceadef0987a764
Imad Manni <128246543+ImadManni@users.noreply.github.com>
2026-06-18 16:56:36 +0100

    Delete scripts directory

--- scripts/_git_status_dump.ps1
+++
@@ -1,3 +0,0 @@
-Set-Location "C:\Users\DELL\Desktop\Automated CV Analysis from Gmail to HR Email"
-git status --short | Out-File -Encoding utf8 git-status-out.txt
-git diff --stat | Out-File -Encoding utf8 git-diff-stat.txt
